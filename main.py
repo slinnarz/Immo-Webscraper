1f3bc1bba1933e2761998172eaf32fbc470e6b99
S. Linnarz <s.linnarz@web.de>
2019-01-21 16:37:42 +0100

    Updated build_URL function
    Started to create a function that builds URLs so that search filters can be applied easily and a loop can be written that iterates over search result pages.

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 
+
+'''
+To DOs:
+    - write loop that builds URLs for each result page and gets the data
+    - save raw data and result-file in a designated result folder
+    - tidy up the code
+'''
+
+
 ################# DEFINE FUNCTIONS #####################
 
 def simple_get(url):
@@ -38,7 +47,7 @@ def is_good_response(resp):
     
 def log_error(e):
     """
-    Is is always a good idea to log errors.
+    It is always a good idea to log errors.
     This function just prints them, but you can
     make it do anything.
     """
@@ -53,8 +62,74 @@ def save_html(inputData, filename):
     with open(filename, "w") as file:
         file.write(pretty_html)
 
-def build_URL():
-    "https://www.immobilienscout24.de/Suche/S-T/Wohnung-
+def build_URL(page, housingType, province, city, roomMin, roomMax,
+              sizeMin, sizeMax, priceMin, priceMax):
+    '''
+    Care: URL of first site of search results differs from other pages.
+    If first page is visited via setting the page counter to 1, search results
+    on page 1 are different.
+    '''
+    url = ("https://www.immobilienscout24.de/Suche/S-T/" +
+            "P-" + page + "/" +
+            "Wohnung-" + housingType + "/" +
+            province + "/" +
+            city + "/" +
+            "-/" + # find out which filter this is for
+            roomMin + ",00" + "-" + roomMax + ",00" + "/" +
+            sizeMin + ",00" + "-" + sizeMax + ",00" + "/" +
+            "EURO-" +
+            priceMin + ",00" + "-" + priceMax + ",00")
+    return url
+
+def get_items(adress = True, cost = True, rooms = True, size = True):
+    '''
+    Get values for specified item from html-file.
+    Allowed inputs (must be str):
+        adress
+        cost
+        rooms
+        size
+    '''
+    if adress == True:
+        itemSelection = html.select('div > button div')
+        ADDcol = list()
+        for i, j in enumerate(itemSelection, 1):
+            ADDcol.append(j.getText())
+        ADDcol = ADDcol[1:len(ADDcol)]
+        # filter empty list entries
+        ADDcol = list(filter(None, ADDcol))
+    if cost == True:
+        itemSelection = html.find_all(class_="font-nowrap font-line-xs")
+        COSTcol = list()
+        for i, j in enumerate(itemSelection, 1):
+            if (i+2) % 3 == 0 and i != 0:
+                COSTcol.append(j.getText())
+    if rooms == True:
+        itemSelection = html.find_all(class_="font-nowrap font-line-xs")
+        ROOMcol = list()
+        for i, j in enumerate(itemSelection, 1):
+            if i % 3 == 0 and i != 0:
+                # Zieht den Text aus dem Tag und speichert erstes Stringelement in Liste
+                ROOMcol.append(j.getText()[0])
+    if size == True:
+        itemSelection = html.find_all(class_="font-nowrap font-line-xs")
+        SIZEcol = list()
+        for i, j in enumerate(itemSelection, 1):
+            if (i+1) % 3 == 0 and i != 0:
+                SIZEcol.append(j.getText())
+    return ADDcol, COSTcol, ROOMcol, SIZEcol
+
+# link examples
+'''
+Some examples of different search result links for a quick lookup.
+For rent, just city, no other filters, page 1:
+"https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Nordrhein-Westfalen/Koeln?enteredFrom=one_step_search"
+For rent, filtered for spans of rent, qm, rooms, page 1:
+"https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Nordrhein-Westfalen/Koeln/-/2,00-45,00/30,00-110,00/EURO-500,00-2000,00?enteredFrom=result_list"
+For rent, filtered for spans of rent, qm, rooms, page 2:
+"https://www.immobilienscout24.de/Suche/S-T/P-2/Wohnung-Miete/Nordrhein-Westfalen/Koeln/-/2,00-45,00/30,00-110,00/EURO-500,00-2000,00"
+'''
+        
 
 ############### SEARCH SETTINGS ##############
 
@@ -71,24 +146,8 @@ sizeMax = str(80)
 priceMin = str(500) # don't forget "EURO" and ",00" in link!
 priceMax = str(1000)
 
-# link examples
-'''
-Some examples of different search result links for a quick lookup.
-For rent, just city, no other filters, page 1:
-"https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Nordrhein-Westfalen/Koeln?enteredFrom=one_step_search"
-For rent, filtered for spans of rent, qm, rooms, page 1:
-"https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Nordrhein-Westfalen/Koeln/-/2,00-45,00/30,00-110,00/EURO-500,00-2000,00?enteredFrom=result_list"
-For rent, filtered for spans of rent, qm, rooms, page 2:
-"https://www.immobilienscout24.de/Suche/S-T/P-2/Wohnung-Miete/Nordrhein-Westfalen/Koeln/-/2,00-45,00/30,00-110,00/EURO-500,00-2000,00"
-'''
 
 ################# SCRIPT #####################
-
-
-
-
-
-
 
 # get date and time
 currDateTime = datetime.datetime.now()
@@ -96,59 +155,17 @@ currDateTime = datetime.datetime.now()
 raw_html = simple_get("https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Nordrhein-Westfalen/Koeln/-/-/-/EURO--1000,00?enteredFrom=one_step_search")
 # save raw html
 save_html(raw_html, "immoscout" + "_" +
-          currDateTime.strftime("%Y%m%d_%H%M") +
+          currDateTime.strftime("%Y%m%d_%H%M") + "_" +
+          #"resultPage" + page + # Zeile aktivieren, wenn per Schleife alle Ergebnisseiten abgefragt werden sollen
           ".html")
 # parse html
 html = BeautifulSoup(raw_html, "html.parser")    
     
-# Suche von tags mit Adressen
-ADDselection = html.select('div > button div')
-ADDsearch = html.find_all("div", class_="font-ellipsis")
-
-for i, j in enumerate(ADDselection, 1):
-#    if i % 3 == 0 and i != 0:
-#        continue
-    print(i, j.getText())
-    
-for i, j in enumerate(ADDsearch, 1):
-#    if i % 3 == 0 and i != 0:
-#        continue
-    print(i, j.getText())
-
-# Suche von tags mit Mietpreisen und Quadratmeterzahlen
-COSTsearch = html.find_all(class_="font-nowrap font-line-xs")
-
-for i, search in enumerate(COSTsearch, 1):
-#    if i % 3 == 0 and i != 0:
-#        continue
-    print(i, search.getText())
-
-# Liste mit Zimmeranzahl
-ROOMcol = list()
-for i, j in enumerate(COSTsearch, 1):
-    if i % 3 == 0 and i != 0:
-        # Zieht den Text aus dem Tag und speichert erstes Stringelement in Liste
-        ROOMcol.append(j.getText()[0])
-
-# Liste mit Kaltmietpreisen
-COSTcol = list()
-for i, j in enumerate(COSTsearch, 1):
-    if (i+2) % 3 == 0 and i != 0:
-        COSTcol.append(j.getText())
-
-# Liste mit Quadratmeterzahlen 
-SIZEcol = list()
-for i, j in enumerate(COSTsearch, 1):
-    if (i+1) % 3 == 0 and i != 0:
-        SIZEcol.append(j.getText())
-
-# list with adresses        
-ADDcol = list()
-for i, j in enumerate(ADDselection, 1):
-    ADDcol.append(j.getText())
-ADDcol = ADDcol[1:len(ADDcol)]
-# filter empty list entries
-ADDcol = list(filter(None, ADDcol))
+# get text data from html
+ADDcol = get_items()[0]
+ROOMcol = get_items()[1]
+COSTcol = get_items()[2]
+SIZEcol = get_items()[3]
 
 # Daten in Dataframe zusammenfassen
 data = {'Quadratmeter':SIZEcol, 'Zimmerzahl':ROOMcol, 'Kaltmiete':COSTcol, 'Adresse':ADDcol}
@@ -164,23 +181,7 @@ df.to_csv('webData.csv')
 # Code-Tests #
 ##############   
     
-#data = {'Quadratmeter':SIZEcol, 'Zimmerzahl':ROOMcol, 'Kaltmiete':COSTcol}
-#df = pd.DataFrame(data) 
-#
-#    
-#    
-#    
-#data = pd.DataFrame()
-#for i, search in enumerate(COSTsearch, 1):
-#    if i % 3 == 0 and i != 0:
-#        print(i, data.append(search.getText()))
-#
-#
-#ROOMcol = list()
-#for i, j in enumerate(COSTsearch, 1):
-#    if i % 3 == 0 and i != 0:
-#        ROOMcol.append(j.getText()[0])
-#    print(i, j.getText())
+
 
 
     
